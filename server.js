38642d47e1439a5f57d22dba2f203a5f98968fbb
Emmanuel Tanifum <etantanifum@gmail.com>
2019-11-05 11:22:06 -0500

    downloaded dependencies and got endpoint to get account functional

--- server.js
+++ server.js
@@ -5,5 +5,18 @@ const db = require('./data/dbConfig.js');
 const server = express();
 
 server.use(express.json());
+server.get('/', (req, res) => {
+    res.send(`<h1>Hakuna Matata</h1>`)
+});
+
+server.get('/api/accounts', (req, res) => {
+    db('Accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'failed to get accounts'})
+        })
+})
 
 module.exports = server;
